b3519e029c9dc67c4b33437ca738717779e82724
Na2Cr2O7 <SchoolF21@outlook.com>
2026-03-11 21:43:28 +0800

    2

---
+++ src/resource.h
@@ -0,0 +1,32 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 res.rc 使用
+//
+#define BUILD_TOPLEVEL                  0
+#define VER_DEBUG                       0
+#define CNF_DIR_EXISTS                  0x0001
+#define BUILD_RECURSING                 1
+#define CNF_ISDIRECTORY                 0x0002
+#define BUILD_NORECURSE                 2
+#define FIND_DIRS                       0x0010
+#define OPER_MKDIR                      0x0100
+#define CCHPATHMAX                      260
+#define OPER_RMDIR                      0x0200
+#define ATTR_ATTRIBS                    0x200
+#define OPER_DOFILE                     0x0300
+#define OPER_ERROR                      0x0400
+#define ATTR_COPIED                     0x400
+#define IDS_SUGGESTBEGIN                1100
+#define ATTR_DELSRC                     0x800
+#define OPER_MASK                       0x0F00
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
